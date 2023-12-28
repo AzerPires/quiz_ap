6f9a8935f9a851e88ba89f00c1c7002021f0db14
AzerPires <azerpg@gmail.com>
2023-12-28 18:37:09 -0500

    texto em frances

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,10 +1,16 @@
 import Menu from "./Menu";
+import { Link } from 'react-router-dom';
 
 function Quiz() {
   return (
     <div>      
       <Menu />
       <p>QUIZ APP</p>
+      <div className="button-container">
+        <Link to="Accueil">
+          <button className="button">Accueil</button>
+        </Link>
+      </div>
     </div>
   );
 }
