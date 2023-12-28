c34ea92ecb7999b83c5ae5d5f6d98c8886d79a61
AzerPires <azerpg@gmail.com>
2023-12-28 11:37:41 -0500

    inserindo botao

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,13 +1,18 @@
 import Menu from "./Menu";
+import { Link } from 'react-router-dom';
+
 
 
 function Accueil() {
     return (
-        <div>            
-            <Menu />
-            <p>Voce está entrando no divertido mundo do Quiz App, uma aplicação aonde vc poderá testar seus conhecimentos.</p>
+        <div className="accueil-container">
+          <Menu />
+            <p>Voce está entrando no divertido mundo do Quiz App, uma aplicação onde você poderá testar seus conhecimentos.</p>
+            <Link to="/">
+            <button>Ir para Quiz</button>   
+            </Link>     
         </div>
-    )
-}
+      );
+    }
 
 export default Accueil
