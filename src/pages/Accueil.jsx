593ce91971ac4dd1e54f56e6e337b6f1db22cf9d
AzerPires <azerpg@gmail.com>
2023-12-28 16:49:59 -0500

    melhorando configuracao

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom';
 
 
 function Accueil() {
-    return (
-        <div className="accueil-container">
-          <Menu />
-            <p>Voce está entrando no divertido mundo do Quiz App, uma aplicação onde você poderá testar seus conhecimentos.</p>
-            <Link to="/">
-            <button>Ir para Quiz</button>   
-            </Link>     
-        </div>
-      );
-    }
+  return (
+    <div className="accueil-container">
+      <Menu />
+      <div className="text">Voce está entrando no divertido mundo do Quiz App. <br /> Clique no botão abaixo e teste seus conhecimentos em linguagens de programação.
+      </div>
+      <div className="button-container">
+        <Link to="/">
+          <button className="button">Ir para Quiz</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default Accueil
